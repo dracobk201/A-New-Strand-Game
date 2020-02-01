2ffa0fd03476ef20e88f78ff922f269e1f982f9f
Erick Subero <dracobk201@gmail.com>
2020-02-01 11:20:39 -0400

    Level 0 y UI básica
    - Bridge y Zone data creada para el Level 0.
    - Organización de prefabs.
    - UI Básica añadida.
    - Text Mesh Pro añadido.

---
+++ Assets/Scripts/UI/Gameplay/GameplayCanvasActions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameplayCanvasActions : MonoBehaviour
+{
+    [SerializeField] private IntReference WoodPool;
+    [SerializeField] private IntReference WaterPool;
+    [SerializeField] private IntReference EnergyPool;
+    [SerializeField] private TextMeshProUGUI woodPoolText;
+    [SerializeField] private TextMeshProUGUI waterPoolText;
+    [SerializeField] private TextMeshProUGUI energyPoolText;
+    
+    private void Start()
+    {
+        UpdatePools();
+    }
+
+    public void UpdatePools()
+    {
+        woodPoolText.text = string.Format("{0}",WoodPool.Value);
+        waterPoolText.text = string.Format("{0}",WaterPool.Value);
+        energyPoolText.text = string.Format("{0}",EnergyPool.Value);
+    }
+}
