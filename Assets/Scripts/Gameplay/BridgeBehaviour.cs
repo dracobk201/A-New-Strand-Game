77e20da90c62e637ac3e2df507050c84ce732393
Erick Subero <dracobk201@gmail.com>
2020-02-01 02:41:35 -0400

    Reparación de puentes
    - Los puentes ya se pueden reparar.
    - Creada la data para los Pools y los bridges.

---
+++ Assets/Scripts/Gameplay/BridgeBehaviour.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeBehaviour : MonoBehaviour
+{
+    [Header ("Pools")]
+    [SerializeField]
+    private IntReference WoodPool;
+    [SerializeField]
+    private IntReference WaterPool;
+    [SerializeField]
+    private IntReference EnergyPool;
+    [Header ("Needs")]
+    [SerializeField]
+    private IntReference WoodNeeded;
+    [SerializeField]
+    private IntReference WaterNeeded;
+    [SerializeField]
+    private IntReference EnergyNeeded;
+    [SerializeField]
+    private GameEvent BridgeRepaired;
+    [SerializeField]
+    private Collider2D otherCollider;
+    private bool playerInside;
+    private bool thisZoneIsRepaired;
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag(Global.PLAYERTAG))
+            playerInside = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag(Global.PLAYERTAG))
+            playerInside = false;
+    }
+
+    public void RepairToolTriggered()
+    {
+        if (playerInside && otherCollider != null && !thisZoneIsRepaired)
+            RepairBridge(otherCollider);
+    }
+
+    private void RepairBridge(Collider2D coll)
+    {
+        if (WoodNeeded.Value <= WoodPool.Value && 
+            WaterNeeded.Value <= WaterPool.Value && 
+            EnergyNeeded.Value <= EnergyPool.Value)
+        {
+            WoodPool.Value -= WoodNeeded.Value;
+            WaterPool.Value -= WaterNeeded.Value;
+            EnergyPool.Value -= EnergyNeeded.Value;
+            //Disparar Animación
+            coll.enabled = false;
+            print("Me reparé");
+            thisZoneIsRepaired = true;
+            BridgeRepaired.Raise();
+        }
+    }
+}
