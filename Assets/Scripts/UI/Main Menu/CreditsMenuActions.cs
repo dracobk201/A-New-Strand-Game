96a23fbda37da808e0908f71c49a6f9dd0f5319b
Erick Subero <dracobk201@gmail.com>
2020-02-01 17:10:20 -0400

    Main Menu
    - Main menu actualizado.
    - avatars añadidos.
    - Fuentes de TextMeshPro añadidas.

---
+++ Assets/Scripts/UI/Main Menu/CreditsMenuActions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsMenuActions : MonoBehaviour
+{
+    public void OpenDracoTwitter()
+    {
+        Application.OpenURL("https://twitter.com/dracobk201");
+    }
+
+    public void OpenKyuuTwitter()
+    {
+        Application.OpenURL("https://twitter.com/KyuuRocks");
+    }
+    
+    public void OpenMiguelFacebook()
+    {
+        Application.OpenURL("https://www.facebook.com/pixel8mike/");
+    }
+    
+    public void OpenDracoWeb()
+    {
+        Application.OpenURL("https://www.ericksubero.com/");
+    }
+    
+    public void OpenKyuuInstagram()
+    {
+        Application.OpenURL("https://www.instagram.com/kyuurocks_art/");
+    }
+
+    public void OpenMiguelInstagram()
+    {
+        Application.OpenURL("https://www.instagram.com/pixel8mike/");
+    }
+}
