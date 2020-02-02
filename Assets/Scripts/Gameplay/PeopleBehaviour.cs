af5cfd3a5d8546d6479fb4b65a92eef6c646ba57
Erick Subero <dracobk201@gmail.com>
2020-02-02 12:05:48 -0400

    Problemas de personas
    - Ahora se voltean.
    - Ahora se instancian en el piso.
    - Ahora no se giran raro.

--- Assets/Scripts/Gameplay/PeopleBehaviour.cs
+++ Assets/Scripts/Gameplay/PeopleBehaviour.cs
@@ -13,6 +13,7 @@ public class PeopleBehaviour : MonoBehaviour
     [SerializeField]
     private FloatReference PersonTimeOfLife;
     private Vector3 direction;
+    private Vector3 scale;
     #endregion
 
     private void OnEnable()
@@ -40,15 +41,17 @@ public class PeopleBehaviour : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D col)
     {
-        foreach (var impact in col.contacts)
+        Debug.Log(string.Format("{0}-{1}",col.collider.name,col.collider.tag));
+        if (col.collider.CompareTag(Global.BLOCKERTAG))
         {
-            if (impact.otherCollider.CompareTag(Global.BLOCKERTAG))
-            {
-                if (direction == Vector3.right)
-                    direction *=-1;
-                else
-                    direction = Vector3.right;
-            }
+            if (direction == Vector3.right)
+                direction *=-1;
+            else
+                direction = Vector3.right;
+                
+            scale = transform.localScale;
+            scale.x *= -1f;
+            transform.localScale = scale;
         }
     }
 }
