49384771e161ce0cf3b750407372aa419cdb1056
Erick Subero <dracobk201@gmail.com>
2020-02-04 21:38:28 -0400

    Créditos
    - "Arreglado" los enlaces de los créditos.

--- Assets/Scripts/UI/Main Menu/CreditsMenuActions.cs
+++ Assets/Scripts/UI/Main Menu/CreditsMenuActions.cs
@@ -1,46 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CreditsMenuActions : MonoBehaviour
 {
-    public void OpenDracoTwitter()
+    public void OpenWeb(int index)
     {
-        Application.OpenURL("https://twitter.com/dracobk201");
-    }
-
-    public void OpenKyuuTwitter()
-    {
-        Application.OpenURL("https://twitter.com/KyuuRocks");
-    }
-    
-    public void OpenMiguelFacebook()
-    {
-        Application.OpenURL("https://www.facebook.com/pixel8mike/");
-    }
-    
-    public void OpenDracoWeb()
-    {
-        Application.OpenURL("https://www.ericksubero.com/");
-    }
-    
-    public void OpenKyuuInstagram()
-    {
-        Application.OpenURL("https://www.instagram.com/kyuurocks_art/");
-    }
-
-    public void OpenMiguelInstagram()
-    {
-        Application.OpenURL("https://www.instagram.com/pixel8mike/");
-    }
-
-    public void OpenSamuelInstagram()
-    {
-        Application.OpenURL("https://www.instagram.com/samuelnavas_/");
-    }
-
-    public void OpenSamuelSoundcloud()
-    {
-        Application.OpenURL("https://soundcloud.com/navassamuel");
+        switch (index)
+        {
+            case 0:
+                Application.OpenURL("https://twitter.com/dracobk201");
+                break;
+            case 1:
+                Application.OpenURL("https://twitter.com/KyuuRocks");
+                break;
+            case 2:
+                Application.OpenURL("https://www.facebook.com/pixel8mike/");
+                break;
+            case 3:
+                Application.OpenURL("https://www.ericksubero.com/");
+                break;
+            case 4:
+                Application.OpenURL("https://www.instagram.com/kyuurocks_art/");
+                break;
+            case 5:
+                Application.OpenURL("https://www.instagram.com/pixel8mike/");
+                break;
+            case 6:
+                Application.OpenURL("https://www.instagram.com/samuelnavas_/");
+                break;
+            case 7:
+                Application.OpenURL("https://soundcloud.com/navassamuel");
+                break;
+        }
     }
 }
